be73b149fa49040505f33b59595f754fe34a0061
Alexandr <sanya.bolotin.96@mail.ru>
2018-07-03 11:13:02 +0300

    Вставил картинку через функцию

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<h4>Главная страницы темы создана</h4>
 	</div>
 	<div class="container-fluid">
-		<a href="#"><img src="/images/gnome_1.png" alt="gnome"/></a>
+		<a href="#"><img src="<?php bloginfo('template_url'); ?>/images/gnome_1.png" alt="gnome"/></a>
 		<p class="text-both">
 		Содержание моей темы. Небольшой текст описывающий тему.
 		</p>
@@ -16,6 +16,7 @@
 	<div class="container-fluid">
 				<?php if ( have_posts() ): ?>
 					<?php while ( have_posts() ): the_post(); ?>
+						<h1>	<?php the_title(); ?> </h1>
 						<p>	<?php the_content(); ?> </p>
 					<?php endwhile; ?>
 				<?php endif; ?>
