f96b7df0f0faf1a56201804075c6fcbe0a2e0eee
Alexandr <sanya.bolotin.96@mail.ru>
2018-07-03 11:48:54 +0300

    Добавил вывод контента страниц

--- page.php
+++ page.php
@@ -1,16 +1,13 @@
 <?php get_header(); ?>
-	<div>
-		<main>
-
-		<?php
-		while ( have_posts() ) : the_post();
-
-			get_template_part( 'content', 'page' );
-
-		endwhile;
-		?>
-
-		</main>
-	</div>
+	<section>
+		<div class="container-fluid">
+					<?php if ( have_posts() ): ?>
+						<?php while ( have_posts() ): the_post(); ?>
+							<h1>	<?php the_title(); ?> </h1>
+							<p>	<?php the_content(); ?> </p>
+						<?php endwhile; ?>
+					<?php endif; ?>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
