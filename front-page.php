7559ab55c2df431cc310b9c7c4137e67942ee585
Alexandr <sanya.bolotin.96@mail.ru>
2018-07-04 10:56:30 +0300

    Создал файл для главной страницы

---
+++ front-page.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+<section>
+	<div class="container-fluid">
+		<h1 class="text-center">Главная страницы темы создана</h1>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+			</div>
+			
+			<div class="col-md-4">
+				<a href="#"><img src="<?php bloginfo('template_url'); ?>/images/gnome_1.png" alt="gnome"/></a>
+				<p class="text-both">
+				Содержание моей темы. Небольшой текст описывающий тему.
+				</p>
+			</div>
+			
+			<div class="col-md-4">
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container-fluid">
+				<?php if ( have_posts() ): ?>
+					<?php while ( have_posts() ): the_post(); ?>
+						<h1><?php the_title(); ?> </h1>
+						<p>	<?php the_content(); ?> </p>
+					<?php endwhile; ?>
+				<?php endif; ?>
+	</div>
+</section>
+<?php get_footer(); ?>
