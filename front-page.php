719ef4bf6ca2ba9672e04388ac174e5bccf4c8f3
Alexandr <sanya.bolotin.96@mail.ru>
2018-07-04 23:56:29 +0300

    Добавил ссылки на посты

--- front-page.php
+++ front-page.php
@@ -25,7 +25,7 @@
 	<div class="container-fluid">
 				<?php if ( have_posts() ): ?>
 					<?php while ( have_posts() ): the_post(); ?>
-						<h1><?php the_title(); ?> </h1>
+						<h1><a href="<?php the_permalink();?>"> <?php the_title(); ?></a> </h1>
 						<p>	<?php the_content(); ?> </p>
 						<?php if ( has_post_thumbnail()):the_post_thumbnail(); ?>
 						<?php endif; ?>	
