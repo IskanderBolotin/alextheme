56ad98fdd22bf568c09ec3ad2665f3187404c749
Alexandr <sanya.bolotin.96@mail.ru>
2018-07-04 11:30:09 +0300

    Добавил вывоб картинкок к посту

--- front-page.php
+++ front-page.php
@@ -27,8 +27,11 @@
 					<?php while ( have_posts() ): the_post(); ?>
 						<h1><?php the_title(); ?> </h1>
 						<p>	<?php the_content(); ?> </p>
+						<?php if ( has_post_thumbnail()):the_post_thumbnail(); ?>
+						<?php endif; ?>	
 					<?php endwhile; ?>
 				<?php endif; ?>
+							
 	</div>
 </section>
 <?php get_footer(); ?>
