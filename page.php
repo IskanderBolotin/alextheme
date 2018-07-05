f989b0a279f99a3c894d97ef84d13a8c488fb910
Alexandr <sanya.bolotin.96@mail.ru>
2018-07-05 10:41:57 +0300

    Изменил вывод содержания страницы

--- page.php
+++ page.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
 	<section>
 		<div class="container-fluid">
-					<?php if ( have_posts() ): ?>
-						<?php while ( have_posts() ): the_post(); ?>
-							<h1>	<?php the_title(); ?> </h1>
-							<p>	<?php the_content(); ?> </p>
-						<?php endwhile; ?>
-					<?php endif; ?>
+					<?php the_post(); ?>
+					<h1><?php the_title();?></h1>
+					<p><?php the_content();?></p>
+					<?php if ( has_post_thumbnail()):the_post_thumbnail(); ?>
+					<?php endif; ?>	
 		</div>
 	</section>
 
